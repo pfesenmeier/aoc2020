d23af494c383048dea4d11365f8b360980dad444
Paul Fesenmeier <pfesenmeier@gmail.com>
2020-12-02 19:49:52 -0500

    Solve Day 2, Problem 2

--- 2/2.ts
+++ 2/2.ts
@@ -2,48 +2,22 @@ import fileToList from '../denoUtils/fileToList.ts';
 
 const list = await fileToList('input.txt', String);
 
-class Range {
-    constructor(range: string) {
-        const nums = range.split('-');
-        this.min = parseInt(nums[0]);
-        this.max = parseInt(nums[1]);
-    }
-    readonly min: number;
-    readonly max: number;
-}
-
-class Char {
-    constructor(charChunk: string) {
-        this.value = charChunk[0];
-    }
-    readonly value: string;
-}
-
-class Password {
-    constructor(password: string) {
-        this.value = password;
-    }
-    readonly value: string;
-}
-
-let valid = 0;
-
+let validCount = 0;
 for (const line of list) {
-    const parts = line.split(' ');
-    const range = new Range(parts[0]);
-    const char = new Char(parts[1]);
-    const password = new Password(parts[2]);
-
-    let freq = 0;
-    for (const c of password.value) {
-      if (c === char.value) {
-          ++freq;
-      }
+    const match = line.match(/([0-9]+)-([0-9]+) ([a-z]): ([a-z]+)/);
+    if (match === null) {
+        throw new Error('regexp failed');
     }
-    
-    if (freq >= range.min && freq <= range.max) {
-        valid++;
+    const index1: number = parseInt(match[1]);
+    const index2: number = parseInt(match[2]);
+    const char: string = match[3];
+    const password: string = match[4];
+
+    if(password[index1 - 1] === char ? password[index2 - 1] !== char : password[index2 - 1] === char) {
+        validCount++;
     }
 }
+console.log(validCount);
 
-console.log(valid);
+
+
