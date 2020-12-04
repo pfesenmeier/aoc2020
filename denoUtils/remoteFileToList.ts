f937475cbf3e2d86b4b7576a12732c4c5e03db56
Paul Fesenmeier <pfesenmeier@gmail.com>
2020-12-03 19:34:15 -0500

    Attempt downloading input. works, but not on aoc

---
+++ denoUtils/remoteFileToList.ts
@@ -0,0 +1,18 @@
+import { readLines } from "https://deno.land/std@0.79.0/io/mod.ts";
+import { readerFromStreamReader } from "https://deno.land/std@0.79.0/io/mod.ts";
+
+interface ParseFunc<T> {
+    (line: string): T;
+}
+
+export default async function proccessFileIntoList<T>(url: string, parseFunc: ParseFunc<T>): Promise<T[]> {
+    const res = await fetch(url);
+    const reader = readerFromStreamReader(res.body!.getReader());
+
+    const list: Array<T> = [];
+    for await (const line of readLines(reader)) {
+      list.push(parseFunc(line));
+    }
+
+    return list;
+}
