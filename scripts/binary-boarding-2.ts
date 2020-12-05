95e24f62eb27748621ce62091530b0b90e707005
Paul Fesenmeier <pfesenmeier@gmail.com>
2020-12-05 16:04:59 -0500

    Day 5 Part 2

--- scripts/binary-boarding-2.ts
+++ scripts/binary-boarding-2.ts
@@ -1,6 +1,12 @@
 // day 5
 
-import { columnNum, list, parseSeat, rowNum } from "./binary-boarding-1.ts";
+import {
+  calculateSeatId,
+  columnNum,
+  list,
+  parseSeat,
+  rowNum,
+} from "./binary-boarding-1.ts";
 
 type Seat = {
   row: number;
@@ -13,31 +19,45 @@ class Airplane {
     for (let i = 0; i < rows; i = i + 1) {
       this.seats.push([]);
     }
+    this.rows = rows;
+    this.columns = columns;
   }
+
   private seats: boolean[][];
+  public rows: number;
+  public columns: number;
+
   fillSeat(seat: Seat): Airplane {
-    if (seat.row === undefined || seat.column === undefined) {
-      console.log(seat.column + " " + seat.row);
-    } else {
-      this.seats[seat.row][seat.column] = true;
-    }
+    this.seats[seat.row][seat.column] = true;
     return this;
+  }
+
+  findEmptySeats(): string[] {
+    const emptySeats = [];
+    for (let i = 0; i < this.rows; i = i + 1) {
+      for (let j = 0; j < this.columns; j = j + 1) {
+        if (this.seats[i][j] === undefined) {
+          emptySeats.push(
+            `Empty seat @ row ${i} column ${j} seatId ${calculateSeatId(i, j)}`,
+          );
+        }
+      }
+    }
+    return emptySeats;
   }
 }
 
-// function fillSeat(airplane: Airplane, seat: Seat): Airplane {
-//     airplane.fillSeat
-//     return airplane;
-// }
+if (import.meta.main) {
+  const filledSeats = list
+    .map((seatBinary) => parseSeat(seatBinary))
+    .map((seat) => ({
+      row: rowNum(seat.row)!,
+      column: columnNum(seat.column)!,
+    }))
+    .reduce(
+      (airplane: Airplane, seat: Seat) => airplane.fillSeat(seat),
+      new Airplane(128, 8),
+    );
 
-const filledSeats = list
-  .map((seatBinary) => parseSeat(seatBinary))
-  .map((seat) => ({ row: rowNum(seat.row)!, column: columnNum(seat.column)! }))
-  .reduce(
-    (airplane: Airplane, seat: Seat) => airplane.fillSeat(seat),
-    new Airplane(128, 8),
-  );
-
-
-// define space, fill it, see what's missing
-// populate plane, check all possible keys, return what's missing
+  console.log(filledSeats.findEmptySeats());
+}
