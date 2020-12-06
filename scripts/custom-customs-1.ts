ed8325738d241e9fe94ca21a2e505074bb74efa4
Paul Fesenmeier <pfesenmeier@gmail.com>
2020-12-06 10:42:14 -0500

    Formatting

--- scripts/custom-customs-1.ts
+++ scripts/custom-customs-1.ts
@@ -10,16 +10,20 @@ type Dict<T> = {
 };
 
 export function groupAnswers(
-  currentGroup: Dict<boolean> = {},
   list: string[] = [],
   processedList: Dict<boolean>[] = [],
+  currentGroup: Dict<boolean> = {},
 ): Dict<boolean>[] {
   if (list.length === 0) return processedList;
-  if (list[0] === '') {
-      return groupAnswers({}, list.slice(1), processedList.concat(currentGroup))
+  const [currentLine, ...restOfLines ] = list;
+
+  if (currentLine === "") {
+    return groupAnswers(restOfLines, processedList.concat(currentGroup));
   }
 
-  list[0].split('').map(letter => currentGroup[letter] = true)
-  return groupAnswers(currentGroup, list.slice(1), processedList);
+  currentLine.split("").map((letter) => currentGroup[letter] = true);
+
+  return groupAnswers(restOfLines, processedList, currentGroup);
 }
 
+console.log(groupAnswers(list));
