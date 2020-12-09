6269e2ac4a0451d551d457634e59ab5cde2d287f
Paul Fesenmeier <pfesenmeier@gmail.com>
2020-12-08 22:39:22 -0500

    Solve Day 8 Part 2

--- scripts/handheld-halting.ts
+++ scripts/handheld-halting.ts
@@ -8,15 +8,14 @@ const list = await remoteFileToList(
 type CMD = "acc" | "jmp" | "nop";
 
 class Instruction {
-  constructor(rawInput: string) {
+  constructor(readonly cmd: CMD, readonly amt: number) {}
+
+  static parse(rawInput: string) {
     const regex = rawInput.match(/([a-z]+) ([\+|-][0-9]+)/);
     if (regex === null) throw new Error("regex failed!");
-
-    this.cmd = regex[1] as CMD;
-    this.amt = parseInt(regex[2]);
+    return new Instruction(regex[1] as CMD, parseInt(regex[2]));
   }
-  readonly cmd: CMD;
-  readonly amt: number;
+
   toString = () => "cmd " + this.cmd + "amt " + this.amt;
 }
 
@@ -27,35 +26,70 @@ function parseInstructions(
   const [currentLine, ...restOfLines] = lines;
   if (currentLine === "") return parsedList;
 
-  const instruction = new Instruction(currentLine);
+  const instruction = Instruction.parse(currentLine);
 
   return parseInstructions(restOfLines, parsedList.concat(instruction));
 }
 
-const instructions = parseInstructions(list);
+const instructions: readonly Instruction[] = parseInstructions(list);
+const indexImmediatelyAfterListCommand = instructions.length;
 
-function findRepeatedInstruction(
-  executedInstructions: Instruction[] = [],
-  currentInstructionIndex = 0,
-  acc = 0,
-): number | undefined {
-    if (currentInstructionIndex in executedInstructions) return acc;
+const makeClosure = (instructions: Instruction[]) =>
+  function findRepeatedInstruction(
+    executedInstructions: Instruction[] = [],
+    currentInstructionIndex = 0,
+    acc = 0,
+  ): [number, boolean] | undefined {
+    if (currentInstructionIndex in executedInstructions) return [acc, false];
+    if (currentInstructionIndex === indexImmediatelyAfterListCommand) {
+      return [acc, true];
+    }
 
     const currentInstruction = instructions[currentInstructionIndex];
     console.log(currentInstruction.toString());
     executedInstructions[currentInstructionIndex] = currentInstruction;
 
-    if (currentInstruction.cmd === 'acc') {
-      return findRepeatedInstruction(executedInstructions, currentInstructionIndex + 1, acc + currentInstruction.amt);
+    if (currentInstruction.cmd === "acc") {
+      return findRepeatedInstruction(
+        executedInstructions,
+        currentInstructionIndex + 1,
+        acc + currentInstruction.amt,
+      );
     }
 
-    if (currentInstruction.cmd === 'jmp') {
-      return findRepeatedInstruction(executedInstructions, currentInstructionIndex + currentInstruction.amt, acc);
+    if (currentInstruction.cmd === "jmp") {
+      return findRepeatedInstruction(
+        executedInstructions,
+        currentInstructionIndex + currentInstruction.amt,
+        acc,
+      );
     }
-    if (currentInstruction.cmd === 'nop') {
-      return findRepeatedInstruction(executedInstructions, currentInstructionIndex + 1, acc);
+    if (currentInstruction.cmd === "nop") {
+      return findRepeatedInstruction(
+        executedInstructions,
+        currentInstructionIndex + 1,
+        acc,
+      );
     }
+  };
+
+function makeLists(checkedIndex = 0): number {
+  const instruction = instructions[checkedIndex];
+  const newList = instructions.slice();
+
+  if (instruction.cmd !== "acc") {
+    if (instruction.cmd === "jmp") {
+      newList[checkedIndex] = new Instruction("nop", instruction.amt);
+    } else {
+      newList[checkedIndex] = new Instruction("jmp", instruction.amt);
+    }
+  }
+
+  const [acc, terminated] = makeClosure(newList)()!;
+
+  if (terminated) return acc;
+
+  return makeLists(++checkedIndex);
 }
 
-const accTotalFirstPass = findRepeatedInstruction();
-console.log(accTotalFirstPass);
+console.log(makeLists());
